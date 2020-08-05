939a69e3aa77ffc0119e030a81b9b6fc1204e0e7
zuzulala0603 <zuzulala0603@gmail.com>
2020-08-06 00:29:47 +0900

    u

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,27 @@
 let searchInput = document.querySelector(".searchM__input");
 
-searchInput.addEventListener("keyup", () => {
+/* searchInput.addEventListener("keyup", () => {
   let input = searchInput.value;
   console.log(input);
   $.getJSON("/static/info/item.json", function (data) {
     console.log(data);
+  });
+});
+ */
+$(function () {
+  var availableCity = ["서울", "부산", "대구", "광주", "울산"];
+
+  $(".searchM__input").autocomplete({
+    source: availableCity,
+
+    select: function (event, ui) {
+      console.log(ui.item);
+    },
+
+    focus: function (event, ui) {
+      return false;
+
+      //event.preventDefault();
+    },
   });
 });
