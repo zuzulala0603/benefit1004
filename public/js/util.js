b4a95b92ca06c12c2ba226ee63dfbf44729450ca
zuzulala0603 <zuzulala0603@gmail.com>
2020-08-15 03:47:31 +0900

    u

--- public/js/util.js
+++ public/js/util.js
@@ -1,5 +1,24 @@
+/* logo */
 let logo = document.querySelector(".header__logo");
-
 logo.addEventListener("click", () => {
   window.location = "/";
 });
+/* benefit block */
+let benefit = document.querySelectorAll(".benefit");
+benefit.forEach((element) => {
+  let benefitNum = element.dataset.benefitnum;
+  console.log(benefitNum);
+  if (benefitNum % 2 == 1) {
+    element.style.flexDirection = "row-reverse";
+  }
+});
+
+/* mobile responsive */
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+if (w < 1025) {
+  let intro = document.getElementById("intro");
+  let img = document.querySelector(".intro__item__imgs");
+  console.log(img);
+  console.log(intro);
+  intro.appendChild(img);
+}
