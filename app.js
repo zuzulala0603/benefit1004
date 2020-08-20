217f34255ad59a01995e4c192f3b01a00f71c340
zuzulala0603 <zuzulala0603@gmail.com>
2020-08-21 04:24:02 +0900

    u

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const favicon = require("serve-favicon");
+const path = require("path");
 app = express();
 //
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 app.use("/static", express.static(__dirname + "/public"));
+app.use(favicon(path.join(__dirname, "public", "img", "favicon.ico")));
 
 //
 const itemRouter = require("./routes/item");
@@ -14,7 +17,10 @@ app.use("/", itemRouter);
 app.get("/", (req, res) => {
   res.render("index");
 });
-
+app.get("/robots.txt", (req, res) => {
+  res.type("text/plain");
+  res.send("User-agent: *\nAllow: /");
+});
 //
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
